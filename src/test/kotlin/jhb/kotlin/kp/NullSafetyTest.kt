4acdc9ccc6bb90e0302e709db6fb7202b6804503
lgwillmore <lgwillmore@gmail.com>
2019-11-05 20:28:38 +0200

    Make null safety tests pass

--- src/test/kotlin/jhb/kotlin/kp/NullSafetyTest.kt
+++ src/test/kotlin/jhb/kotlin/kp/NullSafetyTest.kt
@@ -6,33 +6,31 @@ import org.junit.jupiter.api.Test
 class NullSafetyTest {
 
     @Test
-    fun `using something that might be null in a reference that is not - it is null`(){
+    fun `using something that might be null in a reference that is not - it is null`() {
         val myPossiblyNullString: String? = null
-        Assertions.assertThrows(Exception::class.java){
+        Assertions.assertThrows(Exception::class.java) {
             val myNotNullString: String = myPossiblyNullString!! // Don't us the !! (not null assertion) operator
         }
     }
 
     @Test
-    fun `using something that might be null in a reference that is not - it is not null`(){
+    fun `using something that might be null in a reference that is not - it is not null`() {
         val myPossiblyNullString: String? = "hello"
-        Assertions.assertThrows(Exception::class.java){
-            val myNotNullString: String = myPossiblyNullString!! // Don't us the !! (not null assertion) operator
-        }
+        val myNotNullString: String = myPossiblyNullString!! // Don't us the !! (not null assertion) operator
     }
 
     @Test
-    fun `check for null - conditional`(){
+    fun `check for null - conditional`() {
         val myPossiblyNullString: String? = null
-        val myNotNullString: String = if(myPossiblyNullString != null){
+        val myNotNullString: String = if (myPossiblyNullString != null) {
             myPossiblyNullString
-        }else{
+        } else {
             "default"
         }
     }
 
     @Test
-    fun `check for null - elvis`(){
+    fun `check for null - elvis`() {
         val myPossiblyNullString: String? = null
         val myNotNullString: String = myPossiblyNullString ?: "default"
     }
@@ -45,7 +43,7 @@ class NullSafetyTest {
     }
 
     @Test
-    fun `safe calls`(){
+    fun `safe calls`() {
         val myPossiblyNullString: String? = null
         val thePossibleLength: Int? = myPossiblyNullString?.length
     }

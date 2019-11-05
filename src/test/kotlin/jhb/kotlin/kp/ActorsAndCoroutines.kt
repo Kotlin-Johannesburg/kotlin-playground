1eda34425b4b1edb490a9774e711ebadc2a43172
lgwillmore <lgwillmore@gmail.com>
2019-11-05 22:40:20 +0200

    First few basic coroutine concepts

--- src/test/kotlin/jhb/kotlin/kp/ActorsAndCoroutines.kt
+++ src/test/kotlin/jhb/kotlin/kp/ActorsAndCoroutines.kt
@@ -1,2 +1,96 @@
 package jhb.kotlin.kp
 
+import kotlinx.coroutines.*
+import org.junit.jupiter.api.Test
+
+suspend fun timed(work: suspend () -> Unit) {
+    val start = System.currentTimeMillis()
+    work()
+    println("Elapsed: ${System.currentTimeMillis() - start}")
+}
+
+
+class CoroutinesTest {
+
+    @Test
+    fun `coroutines are concurrent`() = runBlocking {
+        launch {
+            delay(2000)
+            println("I will execute last")
+        }
+        launch {
+            delay(1000)
+            println("I should execute in the middle")
+        }
+        println("I should execute immediately")
+    }
+
+    @Test
+    fun `getting results from a coroutine`() = runBlocking {
+        timed {
+            val deferredResult1 = async {
+                delay(2000)
+                1
+            }
+            val deferredResult2 = async {
+                delay(1000)
+                2
+            }
+            println(println(deferredResult1.await() + deferredResult2.await()))
+        }
+    }
+
+    @Test
+    fun `coroutines are light weight`() = runBlocking {
+        val jobs = List(100_000) {
+            launch {
+                delay(1000)
+                print(".")
+            }
+        }
+        delay(2000)
+        println()
+        println("All done")
+    }
+
+    @Test
+    fun `structured concurrency - if a parent dies, its children are not left dangling`() = runBlocking {
+        val parentJob = launch {
+            println("Parent started")
+            launch {
+                println("Child job 1 started")
+                delay(3000)
+                println("Child job 1 done")
+            }
+            launch {
+                println("Child job 2 started")
+                delay(2000)
+                println("Child job 2 is done")
+            }
+        }
+        delay(1000)
+        println("Cancelling the parent")
+        parentJob.cancel()
+    }
+
+    @Test
+    fun `structured concurrency - errors will propagate up`() = runBlocking {
+        val parentJob = launch {
+            println("Parent started")
+            launch {
+                println("Child job 1 started")
+                delay(3000)
+                println("Child job 1 done")
+            }
+            launch {
+                println("Child job 2 started, but I am going to freak out")
+                delay(2000)
+                throw Exception("Job 2 freaked out")
+            }
+        }
+        parentJob.join()
+        println("Parent done")
+    }
+}
+
+
